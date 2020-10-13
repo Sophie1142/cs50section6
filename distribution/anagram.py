c5cb323032ca2c51699e55e3ac3d860891a54569
Sophie Edouard <sedouard@college.harvard.edu>
2020-10-13 18:56:04 +0000

    pre section

---
+++ distribution/anagram.py
@@ -0,0 +1,14 @@
+# Revisiting Anagram from section 2
+
+# Write a program anagram.c that takes two words as input,
+# and determines whether the words are anagrams of one another.
+# - If two words are an exact match (the strings are identical, ignoring case),
+#     the program should output EXACT MATCH.
+# - If either word contains non-alphabetic characters, the program should
+#     output Alphabetic characters only and return 1.
+# - Otherwise, if two words consist of only alphabetic characters, the
+#     program should check to see if the letters in the first word can
+#     be rearranged (ignoring case) to form the second word. If so,
+#     the program should print ANAGRAM; if not, the program should print NOT ANAGRAM.
+
+# Use the bountiful resources available to you within Python!
---
+++ distribution/phonebook.py
@@ -0,0 +1,9 @@
+# Write a program [phonebook.py](http://phonebook.py) that reads
+# from a CSV file (provided as a command-line argument) using a DictReader and
+# prints out the data on each person in the phone book.
+# The file contains columns name and number, representing
+# each person's name and phone number, respectively.
+
+# As a next step, add some error checking to ensure that
+# (1) the program is run with a command-line argument, and
+# (2) the CSV file contains both a name column and a number column.
